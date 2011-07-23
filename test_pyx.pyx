73e6f995b839f8745014286afbc45ce047b59485
Vitja Makarov <vitja.makarov@gmail.com>
2011-07-23 20:27:20 +0400

    Add C-callback exception testcase

--- test_pyx.pyx
+++ test_pyx.pyx
@@ -52,3 +52,22 @@ cdef class TestCCallback(ev.Timer):
     cdef void ccallback(self, ev.IO io, int event) except *:
         self.timedout = True
         ev.quit()
+
+
+cdef class TestCCallbackExc(ev.Timer):
+    """
+    >>> t = TestCCallbackExc()
+    >>> ev.main()
+    Traceback (most recent call last):
+        ...
+    Error: oops
+    """
+
+    def __init__(self):
+        ev.Timer.__init__(self, .001)
+        self.set_ccallback(<ev.watcher_cb>self.ccallback,
+                           <void *> self)
+        self.start()
+
+    cdef void ccallback(self, ev.IO io, int event) except *:
+        raise ev.Error, "oops"
